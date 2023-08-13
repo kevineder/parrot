6a1d785d754221bd2b34be9d14052b04322ea3cc
Kevin Eder <kevin.eder@gmail.com>
2023-08-13 19:38:39 -0400

    Remove unused import.

--- features/main-screen/src/main/kotlin/hellokevin/features/mainscreen/MainScreen.kt
+++ features/main-screen/src/main/kotlin/hellokevin/features/mainscreen/MainScreen.kt
@@ -3,7 +3,6 @@ package hellokevin.features.mainscreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
